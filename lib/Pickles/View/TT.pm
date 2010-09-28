46c72ff6b52ea0522ba0bdbe3e0acea8cf680492
lestrrat <lestrrat+github@gmail.com>
2010-09-28 16:54:41 +0900

    include the tt instance in to the view instance, and don't create the config hash unless you need to

--- lib/Pickles/View/TT.pm
+++ lib/Pickles/View/TT.pm
@@ -3,8 +3,6 @@ use strict;
 use base qw(Pickles::View);
 use Template;
 
-my $tt;
-
 __PACKAGE__->config( TEMPLATE_EXTENSION => '.html' );
 
 sub new {
@@ -15,20 +13,25 @@ sub new {
 
 sub render {
     my( $self, $c ) = @_;
-    my $config = $self->merge_config( $c );
-    $tt ||= Template->new({
-        ENCODING => 'utf8',
-        UNICODE => 1,
-        ABSOLUTE => 1,
-        INCLUDE_PATH => [
-            $c->config->path_to('view'),
-            $c->config->path_to('view', 'inc'),
-        ],
-        %{$config},
-    });
+
+    my $tt = $self->{tt};
+    if (! $tt) {
+        my $config = $self->merge_config( $c );
+        $tt = $self->{tt} = Template->new({
+            ENCODING => 'utf8',
+            UNICODE => 1,
+            ABSOLUTE => 1,
+            INCLUDE_PATH => [
+                $c->config->path_to('view'),
+                $c->config->path_to('view', 'inc'),
+            ],
+            %{$config},
+        });
+        $self->{suffix} = $config->{TEMPLATE_EXTENSION} || '.tt2';
+    }
     my $template = $c->stash->{'VIEW_TEMPLATE'};
-    unless ( $template =~ /$config->{TEMPLATE_EXTENSION}$/ ) {
-        $template .= $config->{TEMPLATE_EXTENSION};
+    unless ( $template =~ /$self->{suffix}$/ ) {
+        $template .= $self->{suffix};
     }
     $tt->process( $template, {
         %{$c->stash},
