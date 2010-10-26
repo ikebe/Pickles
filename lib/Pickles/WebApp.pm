58b37d56e51e50f59cb7b15222cf78ff5492261b
lestrrat <lestrrat+github@gmail.com>
2010-10-26 18:36:56 +0900

    allow setting context_class

--- lib/Pickles/WebApp.pm
+++ lib/Pickles/WebApp.pm
@@ -1,11 +1,15 @@
 package Pickles::WebApp;
 use strict;
+use base qw(Class::Data::Inheritable);
 use Plack::Util;
 use String::CamelCase qw(camelize);
 
+__PACKAGE__->mk_classdata( 'context_class' => 'Context' );
+
 sub handler {
     my $class = shift;
-    my $context_class = Plack::Util::load_class( 'Context', $class );
+    my $context_class = $class->context_class || 'Context';
+    $context_class = Plack::Util::load_class( $context_class, $class );
     $context_class->setup;
     my $app = sub {
         my $env = shift;
