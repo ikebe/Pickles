5623836a9b493b81dc6fb22862f84172d8b68136
lestrrat <lestrrat+github@gmail.com>
2010-11-06 07:38:43 +0900

    Make the container a real object, no friggin __PACKAGE__ calls

--- lib/Pickles/Container.pm
+++ lib/Pickles/Container.pm
@@ -1,36 +1,43 @@
 package Pickles::Container;
 use strict;
-use base qw(Class::Data::Inheritable);
-
-__PACKAGE__->mk_classdata( __persistent_objects => undef );
-__PACKAGE__->mk_classdata( __components => undef );
 
 sub new {
     my $class = shift;
     bless {
-        objects    => {},
+        objects        => {},
+        scoped_objects => {},
+        components     => {},
     }, $class;
 }
 
-sub objects { $_[0]->{objects} }
+sub Pickles::Container::ScopeGuard::DESTROY {
+    my $self = shift;
+    $self->{container}->clear_scope();
+}
+
+sub new_scope {
+    my $self = shift;
+    return bless { container => $self }, 'Pickles::Container::ScopeGuard';
+}
+
+sub clear_scope {
+    my $self = shift;
+    $self->{scoped_objects} = {};
+}
 
 sub components {
     my $self = shift;
-    my $h = $self->__components();
+use Carp qw(confess);
+confess "poop" if ! ref $self;
+    my $h = $self->{components};
     if (! $h) {
-        $self->__components($h = {});
+        $self->{components} = ($h = {});
     }
     return $h;
 }
 
-sub persistent_objects {
-    my $self = shift;
-    my $h = $self->__persistent_objects();
-    if (! $h) {
-        $self->__persistent_objects($h = {});
-    }
-    return $h;
-}
+sub objects { $_[0]->{objects} }
+sub scoped_objects { $_[0]->{scoped_objects} }
 
 sub register {
     my ($self, $name, $component, $opts) = @_;
@@ -43,21 +50,21 @@ sub register {
         );
         $self->components->{ $name } = \%data;
     } else {
-        $self->persistent_objects->{$name} = $component;
+        $self->objects->{$name} = $component;
     }
 }
 
 sub get {
     my ($self, $name, @args) = @_;
-    my $object = $self->objects->{$name} || $self->persistent_objects->{$name};
+    my $object = $self->{objects}->{$name} || $self->{scoped_objects}->{$name};
     if (! $object) {
         my $data = $self->components->{ $name };
         $object = $self->_construct_object($data, @args);
         if ($object) {
             if ($data->{persistent}) {
-                $self->persistent_objects->{$name} = $object;
-            } else {
                 $self->objects->{$name} = $object;
+            } else {
+                $self->scoped_objects->{$name} = $object;
             }
         }
     }
@@ -85,29 +92,30 @@ Pickles::Container - A Simple Container
     package MyApp::Container;
     use base qw(Pickles::Container);
 
+    # somewhere else in your code
+    my $c = MyApp::Container->new();
+
     # a persistent object (lasts during this process)
     my $object = Foo->new();
-    __PACKAGE__->register( foo => $object );
+    $c->register( foo => $object );
 
     # a per-instance object (lasts only during an instance of 
     # this container is alive)
-    __PACKAGE__->register( bar => sub { Bar->new } );
+    $c->register( bar => sub { Bar->new } );
 
     # a persistent object, lazily instantiated
-    __PACKAGE__->register( baz => sub { Baz->new }, { persistent => 1 } );
+    $c->register( baz => sub { Baz->new }, { persistent => 1 } );
 
-
-    # somewhere else in your code
     {
-        my $c = MyApp::Container->new();
+        my $guard = $c->new_scope;
         my $foo = $c->get('foo');
         my $bar = $c->get('bar'); 
         my $baz = $c->get('baz');
-        # $c goes out of scope
+        # $guard goes out of scope
     }
 
     {
-        my $c = MyApp::Container->new();
+        my $guard = $c->new_scope;
         my $foo = $c->get('foo'); # Same as previous $foo
         my $bar = $c->get('bar'); # DIFFERENT from previous $bar
         my $baz = $c->get('baz'); # Same as previous $baz
--- t/04_container.t
+++ t/04_container.t
@@ -7,15 +7,16 @@ sub PicklesTestDummy::A::new { bless {}, $_[0] };
 sub PicklesTestDummy::B::new { bless {}, $_[0] };
 sub PicklesTestDummy::C::new { bless {}, $_[0] };
 
-Pickles::Container->register( A => PicklesTestDummy::A->new );
-Pickles::Container->register( B => sub { PicklesTestDummy::B->new });
-Pickles::Container->register( C => sub { PicklesTestDummy::C->new }, { persistent => 1 } );
+my $c = Pickles::Container->new();
+$c->register( A => PicklesTestDummy::A->new );
+$c->register( B => sub { PicklesTestDummy::B->new });
+$c->register( C => sub { PicklesTestDummy::C->new }, { persistent => 1 } );
 
 
 {
     my ($A, $B, $C);
     {
-        my $c = Pickles::Container->new();
+        my $scope = $c->new_scope();
         $A = $c->get('A');
         $B = $c->get('B');
         $C = $c->get('C');
@@ -29,7 +30,7 @@ Pickles::Container->register( C => sub { PicklesTestDummy::C->new }, { persisten
     }
         
     {
-        my $c = Pickles::Container->new();
+        my $scope = $c->new_scope();
         is $A, $c->get('A'), "A is the same as previous A";
         isnt $B, $c->get('B'), "B is NOT the same as previous B";
         is $C, $c->get('C'), "C is the same as previous C";
