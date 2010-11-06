d97ef03601a74de0fd3f867cecd3d25e742db955
lestrrat <lestrrat+github@gmail.com>
2010-11-07 08:29:59 +0900

    doc stubs for request/response

--- lib/Pickles/Request.pm
+++ lib/Pickles/Request.pm
@@ -6,3 +6,14 @@ use parent 'Plack::Request';
 1;
 
 __END__
+
+=head1 NAME
+
+Pickles::Request - A Request Object
+
+=head1 SYNOPSIS
+
+    use Pickles::Request;
+    # See Plack::Request
+
+=cut
--- lib/Pickles/Response.pm
+++ lib/Pickles/Response.pm
@@ -5,3 +5,14 @@ use parent 'Plack::Response';
 1;
 
 __END__
+
+=head1 NAME
+
+Pickles::Response - A Response Object
+
+=head1 SYNOPSIS
+
+    use Plickles::Response;
+    # see Plack::Response
+
+=cut
